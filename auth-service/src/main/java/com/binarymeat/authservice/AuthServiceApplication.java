eed9e88c032fce86f0b83e83aac33df6a720bc65
Robert Jakeš <jakesrobert@gmail.com>
2017-01-31 00:04:27 +0100

    add base for jwt

--- auth-service/src/main/java/com/binarymeat/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/binarymeat/authservice/AuthServiceApplication.java
@@ -2,11 +2,14 @@ package com.binarymeat.authservice;
 
 import com.binarymeat.authservice.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,7 +17,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @EnableResourceServer
@@ -22,37 +36,113 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
-	@Configuration
-	@EnableWebSecurity
-	protected static class webSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Configuration
+    @EnableWebSecurity
+    protected static class webSecurityConfig extends WebSecurityConfigurerAdapter {
 
-		@Autowired
-		private CustomUserDetailsService userDetailsService;
+        @Autowired
+        private CustomUserDetailsService userDetailsService;
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-					.authorizeRequests().anyRequest().authenticated()
-					.and()
-					.csrf().disable();
-			// @formatter:on
-		}
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+            // @formatter:on
+        }
 
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService)
-					.passwordEncoder(new BCryptPasswordEncoder());
-		}
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService)
+                    .passwordEncoder(new BCryptPasswordEncoder());
+        }
 
-		@Override
-		@Bean
-		public AuthenticationManager authenticationManagerBean() throws Exception {
-			return super.authenticationManagerBean();
-		}
-	}
+        @Override
+        @Bean
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
+
+        @Autowired
+        private CustomUserDetailsService userDetailsService;
+
+        @Autowired
+        private Environment env;
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+            // TODO persist clients details
+
+            // @formatter:off
+            clients.inMemory()
+                    .withClient("browser")
+                    .authorizedGrantTypes("refresh_token", "password")
+                    .scopes("ui")
+                    .and()
+                    .withClient("account-service")
+                    .secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
+                    .authorizedGrantTypes("client_credentials", "refresh_token")
+                    .scopes("server")
+                    .and()
+                    .withClient("statistics-service")
+                    .secret(env.getProperty("STATISTICS_SERVICE_PASSWORD"))
+                    .authorizedGrantTypes("client_credentials", "refresh_token")
+                    .scopes("server")
+                    .and()
+                    .withClient("notification-service")
+                    .secret(env.getProperty("NOTIFICATION_SERVICE_PASSWORD"))
+                    .authorizedGrantTypes("client_credentials", "refresh_token")
+                    .scopes("server");
+            // @formatter:on
+
+//            .authorizedGrantTypes("implicit","refresh_token", "password", "authorization_code");
+        }
+
+        @Bean
+        public TokenStore tokenStore() {
+            return new JwtTokenStore(jwtTokenEnhancer());
+        }
+
+        @Bean
+        protected JwtAccessTokenConverter jwtTokenEnhancer() {
+            KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), "mySecretKey".toCharArray());
+            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+            converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jwt"));
+            return converter;
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints
+                    .tokenStore(tokenStore()).tokenEnhancer(jwtTokenEnhancer())
+                    .authenticationManager(authenticationManager)
+                    .userDetailsService(userDetailsService);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer
+                    .tokenKeyAccess("permitAll()")
+                    .checkTokenAccess("isAuthenticated()");
+        }
+    }
 }
